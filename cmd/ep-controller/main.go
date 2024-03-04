6fe08908306b8503603705452fdd2add2f23fb0c
Airren <qiang.ren@intel.com>
2024-03-04 15:22:37 +0800

    fix: leader select
    Signed-off-by: Airren <qiang.ren@intel.com>

--- cmd/ep-controller/main.go
+++ cmd/ep-controller/main.go
@@ -7,12 +7,16 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/klog/v2"
 
 	"github.com/multi-cluster-network/ovn-builder/pkg/controller/endpoint"
@@ -20,7 +24,9 @@ import (
 )
 
 var (
-	kubeconfig string
+	kubeconfig      string
+	processIdentify string
+	NAMESPACE       = "nauti"
 )
 
 func main() {
@@ -28,6 +34,7 @@ func main() {
 	flag.StringVar(&kubeconfig, "kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "absolute path to the kubeconfig file")
 	flag.Parse()
 
+	NAMESPACE = os.Getenv("NAMESPACE")
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		// fallback to kube config
@@ -45,13 +52,23 @@ func main() {
 		klog.Fatal(err)
 	}
 
-	factory := informers.NewSharedInformerFactory(clientset, time.Second*5)
+	// todo select master
+	processIdentify = uuid.New().String()
 
+	ctx, cancel := context.WithCancel(context.Background())
 	stop := make(chan struct{})
-	defer close(stop)
-	factory.Start(stop)
 
-	ctx := context.Background()
+	defer func() {
+		close(stop)
+		cancel()
+	}()
+	startLeaderElection(ctx, clientset, stop)
+	select {}
+}
+
+func controllerRun(clientset *kubernetes.Clientset, stop chan struct{}, ctx context.Context) {
+	factory := informers.NewSharedInformerFactory(clientset, time.Second*5)
+	factory.Start(stop)
 	ep := endpoint.NewEndpointController(
 		factory.Core().V1().Pods(),
 		factory.Core().V1().Services(),
@@ -72,6 +89,44 @@ func main() {
 
 	go ep.Run(ctx, 1)
 	go eps.Run(ctx, 1)
+}
 
-	select {}
+// startLeaderElection
+func startLeaderElection(ctx context.Context, clientset *kubernetes.Clientset, stop chan struct{}) {
+	klog.Infof("[%s]creat master lock for election", processIdentify)
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      "nauti-dedinic-controller",
+			Namespace: NAMESPACE,
+		},
+		Client: clientset.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: processIdentify,
+		},
+	}
+	klog.Infof("[%s]start election...", processIdentify)
+	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		Lock:            lock,
+		ReleaseOnCancel: true,
+		LeaseDuration:   10 * time.Second,
+		RenewDeadline:   5 * time.Second,
+		RetryPeriod:     2 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				klog.Infof("[%s] this process is leader，only leader can executor the logic", processIdentify)
+				controllerRun(clientset, stop, ctx)
+			},
+			OnStoppedLeading: func() {
+				klog.Infof("[%s] lose leader", processIdentify)
+				os.Exit(0)
+			},
+			OnNewLeader: func(identity string) {
+				if identity == processIdentify {
+					klog.Infof("[%s]get leader result，the current process is leader", processIdentify)
+					return
+				}
+				klog.Infof("[%s]get leader result，leader is : [%s]", processIdentify, identity)
+			},
+		},
+	})
 }
